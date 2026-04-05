513dfc5920db8d75acdfe81c853cef5928de3c2d
Jaeljc <20241468@itla.edu.do>
2026-04-05 19:00:11 -0400

    agg estilo con css, mejoramos html, funciones para Btn y barra, mejoria visual poniendo limite en los paneles, logica de navegacion, comunicacion entre c# y js para actualizar la barra de direcciones mediante postMessage y PostWebMessageAsString (Enviar comando e URL y sincronzar la barra de dirrecciones).

--- NavionX/NavionX/Form1.cs
+++ NavionX/NavionX/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,72 @@ namespace NavionX
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
+            // Forzar el orden de los views para asegurar que el panel de la interfaz esté siempre encima del navegador
+            panelHeader.SendToBack();
+            webViewBrowser.BringToFront(); 
 
+            // Aseguramos una altura fija 
+            panelHeader.Height = 60;
+            panelHeader.Dock = DockStyle.Top;
 
+            // el contenedor del navegador ocupe el resto
+            webViewBrowser.Dock = DockStyle.Fill;
 
-            +++
+            try
+            {
+                // Inicializa WebView de la interfaz
+                await webViewUI.EnsureCoreWebView2Async(null);
+                string rutaHTML = Path.Combine(Application.StartupPath, "UI", "index.html");
+                if (File.Exists(rutaHTML))
+                {
+                    webViewUI.CoreWebView2.Navigate("file:///" + rutaHTML);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar la interfaz: " + ex.Message);
+            }
+
+            // Inicializa el navegador principal
+            await webViewBrowser.EnsureCoreWebView2Async(null);
+            webViewBrowser.CoreWebView2.Navigate("https://www.google.com");
+            webViewUI.WebMessageReceived += Interfaz_WebMessageReceived;
+
+            // html sabe cuando cambia la url 
+            webViewBrowser.SourceChanged += (s, a) => {
+                webViewUI.CoreWebView2.PostWebMessageAsString(webViewBrowser.Source.ToString());
+            };
+        }
+
+        private void Interfaz_WebMessageReceived(object sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            // Capturamos el mensaje que viene desde el HTML
+            string mensaje = e.TryGetWebMessageAsString();
+
+            // Comprobamos que orden mando el HTML
+            if (mensaje == "back")
+            {
+                if (webViewBrowser.CoreWebView2.CanGoBack) webViewBrowser.CoreWebView2.GoBack();
+            }
+            else if (mensaje == "forward")
+            {
+                if (webViewBrowser.CoreWebView2.CanGoForward) webViewBrowser.CoreWebView2.GoForward();
+            }
+            else if (mensaje == "reload")
+            {
+                webViewBrowser.CoreWebView2.Reload();
+            }
+            else
+            {
+                string url = mensaje;
+                if (!url.StartsWith("http"))
+                {
+                    url = "https://www.google.com/search?q=" + url;
+                }
+                webViewBrowser.CoreWebView2.Navigate(url);
+            }
         }
 
         private void webViewUI_Click(object sender, EventArgs e)
