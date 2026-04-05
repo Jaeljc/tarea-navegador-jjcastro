5e0b49242d34dd1714e717c7ebf473f100311391
Jaeljc <20241468@itla.edu.do>
2026-04-05 18:09:38 -0400

    Agg panel UI, panelBrowser, webview y html de barra + btn

--- NavionX/NavionX/Form1.Designer.cs
+++ NavionX/NavionX/Form1.Designer.cs
@@ -28,20 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.webViewUI = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.webViewBrowser = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panelHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webViewUI)).BeginInit();
+            this.panelContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webViewBrowser)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelHeader.Controls.Add(this.webViewUI);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(1690, 55);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // webViewUI
+            // 
+            this.webViewUI.AllowExternalDrop = true;
+            this.webViewUI.CreationProperties = null;
+            this.webViewUI.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webViewUI.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webViewUI.Location = new System.Drawing.Point(0, 0);
+            this.webViewUI.Name = "webViewUI";
+            this.webViewUI.Size = new System.Drawing.Size(1690, 55);
+            this.webViewUI.TabIndex = 0;
+            this.webViewUI.ZoomFactor = 1D;
+            this.webViewUI.Click += new System.EventHandler(this.webViewUI_Click);
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.Controls.Add(this.webViewBrowser);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(0, 55);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(1690, 883);
+            this.panelContainer.TabIndex = 1;
+            // 
+            // webViewBrowser
+            // 
+            this.webViewBrowser.AllowExternalDrop = true;
+            this.webViewBrowser.CreationProperties = null;
+            this.webViewBrowser.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webViewBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webViewBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webViewBrowser.Name = "webViewBrowser";
+            this.webViewBrowser.Size = new System.Drawing.Size(1690, 883);
+            this.webViewBrowser.TabIndex = 0;
+            this.webViewBrowser.ZoomFactor = 1D;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1690, 938);
+            this.Controls.Add(this.panelContainer);
+            this.Controls.Add(this.panelHeader);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelHeader.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.webViewUI)).EndInit();
+            this.panelContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.webViewBrowser)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Panel panelContainer;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webViewUI;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webViewBrowser;
     }
 }
 
--- NavionX/NavionX/Form1.cs
+++ NavionX/NavionX/Form1.cs
@@ -16,5 +16,18 @@ namespace NavionX
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+            +++
+        }
+
+        private void webViewUI_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
